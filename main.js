947c39381fd1ea5c30aa7897521f09f6a3f99bbb
Phil Rigovanov <phil@rigovanov.ru>
2014-03-08 12:21:53 +0400

    updated

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 if (Meteor.isClient) {
-Template.greetings.rendered = function() {
-  return $(".cs-text").lettering("words").children("span").lettering();
-};
+	Template.greetings.rendered = function() {
+	  return $(".cs-text").lettering("words").children("span").lettering();
+	};
 }
+
 
 if (Meteor.isServer) {
 Meteor.startup(function() {});
